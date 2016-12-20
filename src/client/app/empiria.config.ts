a2699e1a3335f71840ecf40ff2f17bc889be49a4
cyanez <cyanez@ontica.org>
2016-12-20 09:32:00 -0600

    Set temporarily product catalogue DataOperationList items

--- src/client/app/empiria.config.ts
+++ src/client/app/empiria.config.ts
@@ -14,22 +14,143 @@ const TESTS_API_KEY = 'Tr4d3fGZUTzp7pRdUHWTQVcB6TWpLaATAwDPeYzX9yV8eYTNxCe5rsL9d
 
 const DATAOPERATIONLIST: DataOperationDef[] = [
   {
-    'uid': 'System.GetLicense',
-    'url': 'v1/system/license',
-    'method': 'GET'
+    'uid': 'getPDMProductsList',
+    'url': 'v1/product-data/products?searchFor={0}',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.Product',
+    'typeOperation': 'getList'
   },
   {
-    'uid': 'System.GetEndpoints',
-    'url': 'v1/system/api-endpoints',
-    'method': 'GET'
+    'uid': 'getPDMBrandsList',
+    'url': 'v1/product-data/brands?searchFor={0}',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.Brand',
+    'typeOperation': 'getList'
+  },
+  {
+    'uid': 'getPDMBrand',
+    'url': 'v1/product-data/brands/{0}',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.Brand',
+    'typeOperation': 'parse'
+  },
+  {
+    'uid': 'getPDMManufacturerList',
+    'url': 'v1/product-data/manufacturers?searchFor={0}',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.Manufacturer',
+    'typeOperation': 'getList'
+  },
+  {
+    'uid': 'getPDMManufacturer',
+    'url': 'v1/product-data/manufacturers/{0}',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.Manufacturer',
+    'typeOperation': 'parse'
+  },
+  {
+    'uid': 'getPDMCategoryList',
+    'url': 'v1/product-data/product-categories',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.Category',
+    'typeOperation': 'getList'
+  },
+
+  {
+    'uid': 'getPDMCategory',
+    'url': 'v1/product-data/product-categories/{0}',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.Category',
+    'typeOperation': 'parse'
+  },
+  {
+    'uid': 'getPDMCategorySubcategoriesList',
+    'url': 'v1/product-data/product-categories/{0}/sub-categories',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.Category',
+    'typeOperation': 'getSubcategories'
+  },
+  {
+    'uid': 'getPDMSubcategory',
+    'url': 'v1/product-data/product-sub-categories/{0}',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.Subcategory',
+    'typeOperation': 'parse'
+  },
+  {
+    'uid': 'getPDMSubcategoryProductTerms',
+    'url': 'v1/product-data/product-sub-categories/{0}/product-terms',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.Subcategory',
+    'typeOperation': 'getProductTerms'
+  },
+  {
+    'uid': 'getPDMProductTermList',
+    'url': 'v1/product-data/product-terms?searchFor={0}',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.ProductTerm',
+    'typeOperation': 'getList'
+  },
+  {
+    'uid': 'getPDMProductTerm',
+    'url': 'v1/product-data/product-terms/{0}',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.ProductTerm',
+    'typeOperation': 'parse'
+  },
+  {
+    'uid': 'getPDMPresentationUnitList',
+    'url': 'v1/product-data/presentation-units',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.PresentationUnit',
+    'typeOperation': 'getList'
+  },
+  {
+    'uid': 'getPDMPresentationUnit',
+    'url': 'v1/product-data/presentation-units/{0}',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.PresentationUnit',
+    'typeOperation': 'parse'
+  },
+  {
+    'uid': 'getPDMPresentationUnitContentsList',
+    'url': 'v1/product-data/presentation-units/{0}/content-units',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.PresentationUnit',
+    'typeOperation': 'getContentUnits'
+  },
+  {
+    'uid': 'getPDMContentUnit',
+    'url': 'v1/product-data/content-units/{0}',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.ContentUnit',
+    'typeOperation': 'parse'
+  },
+  {
+    'uid': 'getPDMProductsList',
+    'url': 'v1/product-data/products?searchFor={0}',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.Product',
+    'typeOperation': 'getList'
+  },
+  {
+    'uid': 'getPDMProduct',
+    'url': 'v1/product-data/products/{0}',
+    'method': 'GET',
+    'typeName': 'Empiria.Trade.PDM.Product',
+    'typeOperation': 'parse'
   }
+
 ];   // END
 
 export const APP_SETTINGS = {
-  'defaultDataServer':DEFAULT_DATASERVER,
+  'defaultDataServer': DEFAULT_DATASERVER,
   'apiKey': TESTS_API_KEY,
   'initialOperations': DATAOPERATIONLIST
 };
+
+
+
 
 /*
 const TEST_SESSION_TOKEN = '2a3b02e6-9d59-41de-8698-6482ec4e9ce2-' +
